9f4c43b652b3625f259991b5df7ac5a699b73bae
TYLER GANTZ <TYLGANTZ@uat.edu>
2018-01-25 12:58:22 -0700

    Add project files.

---
+++ GuessMyNumberTylerGantz/GuessMyNumberTylerGantz.cpp
@@ -0,0 +1,35 @@
+// GuessMyNumberTylerGantz.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "iostream"
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+	srand(static_cast<unsigned int>(time(0)));
+
+		int secretNumber = rand() % 100 = 1; 
+		
+		int tries = 0;
+		int guess;
+	 
+		cout << "\Welcome to Guess My Number\n\n";
+		do
+		{
+			cout << "Enter a guess: ";
+			cin >> guess;
+			++tries;
+
+			if (guess > secretNumber)
+			{
+				cout << "Too high!\n\n";
+			}
+			else
+	}
+    return 0;
+}
+
